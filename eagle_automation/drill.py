cd1cf5829070a0f0022eca0230d55204ad6d6bd1
Bernard Pratz <guyzmo+github@m0g.net>
2015-11-11 10:29:24 +0000

    Switch drill module to space indent
    Signed-off-by: Bernard Pratz <guyzmo+github@m0g.net>

--- eagle_automation/drill.py
+++ eagle_automation/drill.py
@@ -6,28 +6,27 @@ import sys
 import logging
 log = logging.getLogger('pea').getChild(__name__)
 
+
 def drill():
-	lines = 0
+    lines = 0
 
-	for line in sys.stdin:
-		g = re.match(r" (T[0-9][0-9]) *([0-9.]+)(\w+) *", line)
-		if g:
-			drill, size, unit = g.groups()
-			size = float(size)
+    for line in sys.stdin:
+        g = re.match(r" (T[0-9][0-9]) *([0-9.]+)(\w+) *", line)
+        if g:
+            drill, size, unit = g.groups()
+            size = float(size)
 
-			if unit == 'inch':
-				size = size * 2.54e1
-			elif unit == 'mils':
-				size = size * 2.54e-2
-			elif unit != 'mm':
-				raise Exception("Unknown unit: " + unit)
+            if unit == 'inch':
+                size = size * 2.54e1
+            elif unit == 'mils':
+                size = size * 2.54e-2
+            elif unit != 'mm':
+                raise Exception("Unknown unit: " + unit)
 
-			sys.stdout.write("%s  %.2fmm\n" % (drill, size))
+            sys.stdout.write("%s  %.2fmm\n" % (drill, size))
 
-			lines += 1
+            lines += 1
 
-	if not lines:
-		log.error("no drills found!\n")
-		sys.exit(1)
-
-
+    if not lines:
+        log.error("no drills found!\n")
+        sys.exit(1)
